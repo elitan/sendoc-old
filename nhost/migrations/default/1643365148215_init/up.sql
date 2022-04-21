df12b6ae0ec0441cb49a4c18007e50285f583c9a
Johan Eliasson <johan@eliasson.me>
2022-04-21 10:07:52 +0200

    Update up.sql

--- nhost/migrations/default/1643365148215_init/up.sql
+++ nhost/migrations/default/1643365148215_init/up.sql
@@ -1,4 +1,17 @@
 SET check_function_bodies = false;
+
+CREATE OR REPLACE FUNCTION public.set_current_timestamp_updated_at() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$;
+
 CREATE TABLE public.doc_links (
     id uuid DEFAULT gen_random_uuid() NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
