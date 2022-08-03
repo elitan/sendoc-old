3d1b6e9aa1df6ee13dea66fcb054e04e06782489
Johan Eliasson <johan@eliasson.me>
2022-08-03 22:29:13 +0200

    link

--- pages/contact.page.jsx
+++ pages/contact.page.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export { Page }
 
 function Page() {
-  return <div>Contact me</div>
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+      Contact me
+    </div>
+  )
 }
